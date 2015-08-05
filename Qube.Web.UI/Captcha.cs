8d476a7be484e26e1a268289d8988251996292a5
DARKGuy <darkguy2008@gmail.com>
2015-08-05 12:27:37 -0430

    Added new Qube.Web.UI library, contains Captcha and simple TextBox control

---
+++ Qube.Web.UI/Captcha.cs
@@ -0,0 +1,183 @@
+﻿using Qube.Web.Core;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Qube.Web.UI
+{
+    public class Captcha : HtmlCustomControl, IRequiresSessionState, IValidator
+    {
+        public Captcha()
+            : base("img")
+        {
+            Width = 200;
+            Height = 50;
+        }
+
+        CustomValidator cv;
+        public String ValidationGroup { get; set; }
+        public String ValidationTextBoxID { get; set; }
+        public String ErrorMessage { get; set; }
+        public ValidatorDisplay Display { get; set; }
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            cv = new CustomValidator()
+            {
+                EnableClientScript = false,
+                ValidationGroup = ValidationGroup,
+                ValidateEmptyText = true
+            };
+            cv.ServerValidate += cv_ServerValidate;
+        }
+
+        void cv_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            TextBox txCaptcha = (TextBox)Page.FindControl(ValidationTextBoxID);
+
+            if (txCaptcha.Text.Trim() == HttpContext.Current.Session["Captcha_" + ID].ToString())
+            {
+                HttpContext.Current.Session["Valid_" + ID] = true;
+                args.IsValid = true;
+            }
+            else
+            {
+                Regenerate();
+                args.IsValid = false;
+                txCaptcha.Text = String.Empty;
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            cv.ControlToValidate = ValidationTextBoxID;
+            cv.ErrorMessage = ErrorMessage;
+            cv.Display = Display;
+            Controls.Add(cv);
+
+            if (!Page.IsPostBack)
+                Regenerate();
+        }
+
+        public void Regenerate()
+        {
+            Text = GenerateRandomCode();
+            HttpContext.Current.Session["Valid_" + ID] = false;
+            HttpContext.Current.Session["Captcha_" + ID] = Text;
+            Attributes["src"] = GenerateImage();
+        }
+
+        public String Text { get; set; }
+        public Bitmap Image { get; set; }
+        public new int Width { get; set; }
+        public new int Height { get; set; }
+
+        private string familyName;
+        private Random random = new Random();
+
+        private string GenerateRandomCode()
+        {
+            string s = "";
+            for (int i = 0; i < 6; i++)
+                s = String.Concat(s, this.random.Next(10).ToString());
+            return s;
+        }
+
+        private void SetFamilyName(string name)
+        {
+            try
+            {
+                Font font = new Font(familyName, 12F);
+                familyName = name;
+                font.Dispose();
+            }
+            catch (Exception)
+            {
+                familyName = FontFamily.GenericSerif.Name;
+            }
+        }
+
+        private String GenerateImage()
+        {
+            Image = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(Image);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle r = new Rectangle(0, 0, Width, Height);
+            HatchBrush hbr = new HatchBrush(HatchStyle.SmallConfetti, Color.LightGray, Color.White);
+            g.FillRectangle(hbr, r);
+            float fSize = r.Height + 1;
+            Font f;
+            SizeF size;
+            do
+            {
+                fSize--;
+                f = new Font(familyName, fSize, FontStyle.Bold);
+                size = g.MeasureString(Text, f);
+            } while (size.Width > r.Width);
+
+            StringFormat format = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+            GraphicsPath path = new GraphicsPath();
+            path.AddString(Text, f.FontFamily, (int)f.Style, f.Size, r, format);
+            float v = 4F;
+            PointF[] points =
+            {
+                new PointF(this.random.Next(r.Width) / v, this.random.Next(r.Height) / v),
+                new PointF(r.Width - this.random.Next(r.Width) / v, this.random.Next(r.Height) / v),
+                new PointF(this.random.Next(r.Width) / v, r.Height - this.random.Next(r.Height) / v),
+                new PointF(r.Width - this.random.Next(r.Width) / v,r.Height - 
+                this.random.Next(r.Height) / v)
+            };
+
+            Matrix mx = new Matrix();
+            mx.Translate(0F, 0F);
+            path.Warp(points, r, mx, WarpMode.Perspective, 0F);
+            hbr = new HatchBrush(HatchStyle.LargeConfetti, Color.LightGray, Color.DarkGray);
+            g.FillPath(hbr, path);
+
+            int m = Math.Max(r.Width, r.Height);
+            for (int i = 0; i < (int)(r.Width * r.Height / 30F); i++)
+            {
+                int x = this.random.Next(r.Width);
+                int y = this.random.Next(r.Height);
+                int w = this.random.Next(m / 50);
+                int h = this.random.Next(m / 50);
+                g.FillEllipse(hbr, x, y, w, h);
+            }
+
+            f.Dispose();
+            hbr.Dispose();
+            g.Dispose();
+
+            MemoryStream ms = new MemoryStream();
+            Image.Save(ms, ImageFormat.Png);
+            return "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+        }
+
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)HttpContext.Current.Session["Valid_" + ID];
+            }
+            set
+            {
+                HttpContext.Current.Session["Valid_" + ID] = value;
+            }
+        }
+
+        public void Validate()
+        {
+            cv.Validate();
+        }
+    }
+
+}
---
+++ Qube.Web.UI/QubeTextBox.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
+
+namespace Qube.Web.UI
+{
+
+    public enum EValidationType
+    {
+        Alpha = 1,
+        Numeric,
+        Alphanumeric,
+        Name,
+        Email,
+        Date,
+        Password,
+        Phone
+    }
+
+    public class QubeTextBox : TextBox
+    {
+        private CustomValidator cv;
+        public bool Required { get; set; }
+        public String FieldName { get; set; }
+        public String ErrorMessage { get; set; }
+        public String EmptyErrorMessage { get; set; }
+        public String LongErrorMessage { get; set; }
+        public EValidationType ValidationType { get; set; }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            if (MaxLength == 0)
+                MaxLength = 50;
+
+            cv = new CustomValidator()
+            {
+                ControlToValidate = ID,
+                EnableClientScript = false,
+                ValidationGroup = ValidationGroup,
+                ValidateEmptyText = true
+            };
+            cv.ServerValidate += cv_ServerValidate;
+            Controls.Add(cv);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {            
+            base.OnLoad(e);
+            ErrorMessage = String.IsNullOrEmpty(ErrorMessage) ? "The field <strong>{0}</strong> is invalid" : ErrorMessage;
+            EmptyErrorMessage = String.IsNullOrEmpty(EmptyErrorMessage) ? "The field <strong>{0}</strong> is required" : EmptyErrorMessage;
+            LongErrorMessage = String.IsNullOrEmpty(LongErrorMessage) ? "The field <strong>{0}</strong> is too long" : LongErrorMessage;
+        }
+
+        void cv_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (Required && String.IsNullOrWhiteSpace(args.Value))
+            {
+                args.IsValid = false;
+                cv.ErrorMessage = String.Format(EmptyErrorMessage, FieldName);
+                return;
+            }
+
+            if (args.Value.Length > MaxLength)
+            {
+                args.IsValid = false;
+                cv.ErrorMessage = String.Format(LongErrorMessage, FieldName);
+                return;
+            }
+
+            if (!String.IsNullOrWhiteSpace(args.Value))
+            {
+                switch (ValidationType)
+                {
+                    case EValidationType.Alpha:
+                        if (!Regex.IsMatch(args.Value, @"^[a-záéíóúñA-ZÁÉÍÓÚÑ\-'`´ ]*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Name:
+                        if (!Regex.IsMatch(args.Value, @"^[a-záéíóúñA-ZÁÉÍÓÚÑ\-'`´ ]*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Alphanumeric:
+                        if (!Regex.IsMatch(args.Value, @"^[0-9a-záéíóúñA-ZÁÉÍÓÚÑ\-'`´_,. ]*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Numeric:
+                        if (!Regex.IsMatch(args.Value, "^[0-9]*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Phone:
+                        if (!Regex.IsMatch(args.Value, "^[0-9()-. ]*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Email:
+                        if (!Regex.IsMatch(args.Value, "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"))
+                            args.IsValid = false;
+                        break;
+                    case EValidationType.Date:
+                        DateTime tmp = DateTime.Now;
+                        if (!DateTime.TryParseExact(args.Value, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out tmp))
+                            args.IsValid = false;
+                        break;                        
+                    case EValidationType.Password:
+                        if (!Regex.IsMatch(args.Value, @"^[a-zA-Z0-9\#\!\@\$\&\*\+\-_]{8,15}$"))
+                            args.IsValid = false;
+                        break;
+                }
+                if (!args.IsValid)
+                    cv.ErrorMessage = String.Format(ErrorMessage, FieldName);
+            }
+            
+        }
+    }
+
+}
