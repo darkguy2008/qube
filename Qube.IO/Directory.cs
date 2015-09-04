b8929840ae7a7483590e6f28f777eb8b403ae81a
DARKGuy <darkguy2008@gmail.com>
2015-09-04 00:47:09 -0430

    Added directory extensions calss

---
+++ Qube.IO/Directory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Qube.IO
+{
+    public static class DirectoryUtils
+    {
+        public static Exception LastError { get; private set; }
+        public static void ClearLastError()
+        {
+            LastError = null;
+        }
+
+        // http://www.codeproject.com/Tips/279969/Recursively-Copy-folder-contents-to-another-in-C
+        public static bool CopyContents(string sourcePath, string destinationPath)
+        {
+            string slashEndingSourcePath = sourcePath.EndsWith(@"\") ? sourcePath : sourcePath + @"\";
+            string slashEndingDestinationPath = destinationPath.EndsWith(@"\") ? destinationPath : destinationPath + @"\";
+            try
+            {
+                if (Directory.Exists(slashEndingSourcePath))
+                {
+                    if (!Directory.Exists(slashEndingDestinationPath))
+                        Directory.CreateDirectory(slashEndingDestinationPath);
+
+                    foreach (string files in Directory.GetFiles(slashEndingSourcePath))
+                        new FileInfo(files).CopyTo(slashEndingDestinationPath + new FileInfo(files).Name, true);
+
+                    foreach (string drs in Directory.GetDirectories(slashEndingSourcePath))
+                        if (!CopyContents(drs, slashEndingDestinationPath + new DirectoryInfo(drs).Name))
+                            return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LastError = ex;
+                return false;
+            }
+        }
+    }
+}
---
+++ Qube.IO/DirectoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Qube.IO
+{
+    public static class DirectoryExtensions
+    {
+        public static void Clean(this DirectoryInfo dir)
+        {
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                fi.IsReadOnly = false;
+                fi.Delete();
+            }
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                di.Clean();
+                di.Delete();
+            }
+        }
+    }
+}
