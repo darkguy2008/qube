86716fcfd4b6142a13fd3cd16891c7f54bd5be62
DARKGuy <darkguy2008@gmail.com>
2015-09-21 00:33:16 -0430

    New DateTimeExtensions class

---
+++ Qube.Extensions/DateTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qube.Extensions
+{
+    public static class DateTimeExtensions
+    {
+
+        public static DateTime MinSQLValue(this DateTime d)
+        {
+            return new DateTime(1753, 1, 1);
+        }
+
+        public static DateTime MaxSQLValue(this DateTime d)
+        {
+            return new DateTime(9999, 12, 31);
+        }
+
+    }
+}
