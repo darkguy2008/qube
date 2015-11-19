2e6a7913836cd7b32e11f1d0478a1832bbce0b6d
DARKGuy <darkguy2008@gmail.com>
2015-11-19 02:01:53 -0430

    Changed Qube.Console to Qube.ConsoleApp to avoid Console namespace/class collision. Added new ConsoleApp base class with generic stuff useful for most console apps being developed nowadays.

--- Qube.Console/CommandLine.cs
+++ Qube.Console/CommandLine.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Qube.Console
+namespace Qube.ConsoleApp
 {
     public class CommandLine
     {
---
+++ Qube.Console/ConsoleApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Qube.ConsoleApp
+{
+    // http://stackoverflow.com/a/21425134
+    public abstract class ConsoleApplication
+    {
+
+        public static string AppPath { get { return new FileInfo(Assembly.GetEntryAssembly().Location).DirectoryName + "\\"; } }
+
+        public virtual void Run(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            throw (Exception)e.ExceptionObject;
+        }
+
+    }
+}
